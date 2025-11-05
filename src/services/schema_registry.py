74db99909d448288e92fad68b57da8466fa74b58
babymerlen <mmromadin@gmail.com>
2025-11-06 00:54:23 +0300

    feat: Adds schema registry service to register Avro schemas with a server.

---
+++ src/services/schema_registry.py
@@ -0,0 +1,14 @@
+import requests
+from src.avro.events.generate_tasks_event import GenerateTask
+import json
+
+def register_schema(subject, schema_str, registry_url="http://localhost:8081"):
+    headers = {"Content-Type": "application/vnd.schemaregistry.v1+json"}
+    payload = {"schema": schema_str}
+    response = requests.post(f"{registry_url}/subjects/{subject}/versions",
+                             headers=headers, json=payload)
+    return response.json()
+
+
+avro_schema = json.dumps(GenerateTask.avro_schema_to_python())
+register_schema("task.requests-value", avro_schema)
