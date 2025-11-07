6d8988fac6fa26e9656962f0aaa2521adb8f93d5
babymerlen <mmromadin@gmail.com>
2025-11-07 20:55:54 +0300

    Refactor: Add blank lines to improve code readability in both files.

--- src/services/avro_serialization.py
+++ src/services/avro_serialization.py
@@ -1,10 +1,12 @@
 import io
 from fastavro import schemaless_writer, schemaless_reader
 
+
 def avro_serialize(obj, schema):
     buf = io.BytesIO()
     schemaless_writer(buf, schema, obj)
     return buf.getvalue()
+
 
 def avro_deserialize(data, schema):
     buf = io.BytesIO(data)
--- src/services/schema_registry.py
+++ src/services/schema_registry.py
@@ -2,11 +2,13 @@ import requests
 from src.avro.events.generate_tasks_event import GenerateTask
 import json
 
+
 def register_schema(subject, schema_str, registry_url="http://localhost:8081"):
     headers = {"Content-Type": "application/vnd.schemaregistry.v1+json"}
     payload = {"schema": schema_str}
-    response = requests.post(f"{registry_url}/subjects/{subject}/versions",
-                             headers=headers, json=payload)
+    response = requests.post(
+        f"{registry_url}/subjects/{subject}/versions", headers=headers, json=payload
+    )
     return response.json()
 
 
